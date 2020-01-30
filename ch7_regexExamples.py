bee4aad341599eda00024d2665e25a1532439023
ChinaBackGo <ChinaBackGo@users.noreply.github.com>
2020-01-30 15:19:17 +0800

    adding ch7 examples

---
+++ ch7_regexExamples.py
@@ -0,0 +1,104 @@
+import re
+
+def isPhoneNumber(text):
+    phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+    mo = phoneNumRegex.search(text)
+    print('Phone number found: ' + mo.group())
+    return True
+
+def isPhoneNumberGroup(text):
+    phoneNumRegex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
+    mo = phoneNumRegex.search(text)
+    print('Phone number found: ' + mo.group(1))
+    print('Phone number found: ' + mo.group(2))
+    print('Phone number found: ' + mo.group(0))
+    areaCode, mainNumber = mo.groups()
+    print('Area code ' + areaCode)
+    print('Main number ' + mainNumber)
+    return True
+
+
+message = 'Call me at 415-555-1011 tomorrow. 415-555-9999 is my office.'
+
+isPhoneNumber(message)
+isPhoneNumberGroup(message)
+
+# pipe | example
+heroRegex = re.compile(r'Batman|Tina Fey')
+mo1 = heroRegex.search('Batman and Tina Fey')
+print(mo1.group())
+
+mo2 = heroRegex.search('Tina Fey and Batman')
+print(mo2.group())
+
+batRegex = re.compile(r'Bat(man|mobile|copter|bat)')
+mo3 = batRegex.search('Batmobile lost a wheel')
+print(mo3.group())
+print(mo3.group(1))
+
+# optional Matching zero or one ?
+batRegex = re.compile(r'Bat(wo)?man')
+mo1 = batRegex.search('The Adventures of Batman')
+print(mo1.group())
+
+mo2 = batRegex.search('The Adventures of Batwoman')
+print(mo2.group())
+
+# matching zero or more *
+batRegex = re.compile(r'Bat(wo)*man')
+mo1 = batRegex.search('The Adventures of Batman')
+print(mo1.group())
+mo2 = batRegex.search('The Adventures of Batwoman')
+print(mo2.group())
+mo3 = batRegex.search('The Adventures of Batwowowowoman')
+print(mo3.group())
+
+# matching one or more +
+batRegex = re.compile(r'Bat(wo)+man')
+mo1 = batRegex.search('The Adventures of Batwoman')
+print(mo1.group())
+mo2 = batRegex.search('The Adventures of Batwowowowoman')
+print(mo2.group())
+mo3 = batRegex.search('The Adventures of Batman')
+print(mo3)
+
+# matching specific repetitions {}
+haRegex = re.compile(r'(Ha){3}')
+mo1 = haRegex.search('HaHaHa')
+print(mo1.group())
+
+# greedy and nongreedy
+greedyHaRegex = re.compile(r'(Ha){3,5}')
+mo1 = greedyHaRegex.search('HaHaHaHaHa')
+print(mo1.group())
+
+nongreedyHaRegex = re.compile(r'(Ha){3,5}?')
+mo2 = nongreedyHaRegex.search('HaHaHaHaHa')
+print(mo2.group())
+
+# findAll method
+phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+mo = phoneNumRegex.findall('Cell: 415-555-9999 Work: 212-555-0000')
+print(mo)
+
+phoneNumRegex = re.compile(r'(\d\d\d)-(\d\d\d)-(\d\d\d\d)')
+mo = phoneNumRegex.findall('Cell: 415-555-9999 Work: 212-555-0000')
+print(mo)
+
+# character classes
+xmasRegex = re.compile(r'\d+\s\w+')
+mo = xmasRegex.findall('12 drummers, 11 pipers, 10 lords, 9 ladies, 8 maids, \
+7 swans, 6 geese, 5 rings, 4 birds, 3 hens, 2 doves, 1 partridge')
+print(mo)
+
+# custom character class
+vowelRegex = re.compile(r'[aeiouAEIOU]')
+mo = vowelRegex.findall('RoboCop eats baby food. BABY FOOD.')
+print(mo)
+
+# inverse ^
+vowelRegex = re.compile(r'[^aeiouAEIOU]')
+mo = vowelRegex.findall('RoboCop eats baby food. BABY FOOD.')
+print(mo)
+
+# caret ^ and dollar sign $
